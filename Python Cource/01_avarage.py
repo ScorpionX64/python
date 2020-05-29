f68e287f44196678e15e66c3993e333af9a6dd6b
Jonathan Healing <jonathan@firsttech.digital>
2020-05-29 18:11:34 +0200

    Udemy python 3 course

---
+++ Python Cource/01_avarage.py
@@ -0,0 +1,5 @@
+print("This program calculates the avarage of two numbers")
+number1 = float(input("Please enter number 1: "))
+number2 = float(input("Please enter number 2: "))
+print("The numbers are ", number1, " and ", number2)
+print("The avarage is: ",(number1 + number2)/2)
---
+++ Python Cource/convert km to mile.py
@@ -0,0 +1,4 @@
+print("Convert Km to Ml")
+userNr = float(input("Please inter the Km for convrersion to mile: "))
+mile = userNr / 1.609344
+print(userNr, "Km",round(mile,2),"Miles")
